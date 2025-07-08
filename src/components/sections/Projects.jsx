12e11b9692a11ce22919e71df8ac59dcae638367
Kasim Sabir <41677739+Qasim223@users.noreply.github.com>
2025-07-08 01:05:31 +0100

    Added new Project

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -32,7 +32,35 @@ export const Projects = () => {
               </div>
               <div className="flex justify-between items-center">
                 <a
-                  href="https://onefitsoftware.com/"
+                  href="https://github.com/Qasim223/Ziki"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Details →
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+              <h3 className="text-xl font-bold mb-2">Python FastAPI</h3>
+              <p className="text-grey-400 mb-4">
+                FastAPI Python Backend that is used to pass data from MongoDB to Frontend 
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["Python", "FastAPI", "MongoDB", "GitHub"].map(
+                  (tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all"
+                    >
+                      {tech}
+                    </span>
+                  )
+                )}
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  href="https://github.com/Qasim223/PythonFastAPI"
                   className="text-blue-400 hover:text-blue-300 transition-colors my-4"
                 >
                   View Details →
